f5c2d5c4aa9d15f6b13595a000b4eb3a644b4f9f
Me1k0r <runner-me1k0r@proton.me>
2025-01-05 22:15:51 +0100

    test: sddm - tests with astronaut theme

--- .local/share/bin/sddmtheme.sh
+++ .local/share/bin/sddmtheme.sh
@@ -4,3 +4,4 @@ scrDir=`dirname "$(realpath "$0")"`
 source $scrDir/globalcontrol.sh
 
 sudo cp "${confDir}/sddm/themes/corners/theme.conf" "/usr/share/sddm/themes/corners/theme.conf"
+sudo cp "${confDir}/sddm/themes/astronaut/theme.conf" "/usr/share/sddm/themes/astronaut/Themes/astronaut.conf"
