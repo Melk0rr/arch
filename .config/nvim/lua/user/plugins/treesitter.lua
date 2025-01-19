3552c3649347dad4e43737df52a6d90b1c89a3c5
Me1k0r <runner-me1k0r@proton.me>
2025-01-19 22:54:16 +0100

    edit: nvim - treesitter ensure lang

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -1,4 +1,24 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "cpp",
+          "fish",
+          "go",
+          "json",
+          "lua",
+          "make",
+          "markdown",
+          "python",
+          "rust",
+          "toml",
+          "vim",
+          "yaml"
+        }
+      })
+    end
 }
