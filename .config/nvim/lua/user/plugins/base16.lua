e958e08e2f73d49db4e2781423c64b6202187b68
Me1k0r <runner-me1k0r@proton.me>
2025-01-22 21:19:10 +0100

    new: nvim - base16 color plugin to handle colorscheme with wallbash

---
+++ .config/nvim/lua/user/plugins/base16.lua
@@ -0,0 +1,11 @@
+return {
+  "RRethy/base16-nvim",
+  config = function()
+    require("base16-colorscheme").setup({
+      base00 = '#2E2B2C', base01 = '#FFCCDB', base02 = '#EECCFF', base03 = '#CCE3FF',
+      base04 = '#E69AB3', base05 = '#CC9AE6', base06 = '#9ABDE6', base07 = '#FFCCDB',
+      base08 = '#522936', base09 = '#F0AABE', base0A = '#D9AAF0', base0B = '#AACAF0',
+      base0C = '#E69AB3', base0D = '#CC9AE6', base0E = '#9ABDE6', base0F = '#F0AABE',
+    })
+  end
+}
