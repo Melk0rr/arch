ccb2fc89c1774ccff7c58e044695f40fd8e142fb
Me1k0r <runner-me1k0r@proton.me>
2025-01-23 21:50:32 +0100

    edit: nvim - passed some keybinds to lsp configs

--- .config/nvim/lua/user/plugins/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lspconfig.lua
@@ -1,28 +1,37 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        local lspconfig = require "lspconfig"
+        local lspconfig = require("lspconfig")
+        local on_attach = function(_, bufnr)
+          local opts = { noremap = true, silent = true }
+          local map = function(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+          map('n', "<leader>[", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+          map('n', "<leader>]", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+          map('n', "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+          map('n', "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+          map('n', "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+          map('n', "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+          map('n', "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+          map('n', "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+          map('n', "<leader>q", "<cmd>Telescope diagnostics<CR>", opts)
+          map('n', "<leader>p", "<cmd>Telescope lsp_workspace_symbols<CR>", opts)
+        end
 
-        lspconfig.lua_ls.setup {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { 'vim' },
-                    },
-                },
-            },
-        }
-        lspconfig.bashls.setup {}
-        lspconfig.pylsp.setup {}
-        lspconfig.wgsl_analyzer.setup {}
-        lspconfig.asm_lsp.setup {}
-        lspconfig.clangd.setup {}
-        lspconfig.jdtls.setup {}
-        -- lspconfig.nil_ls.setup {}
-        lspconfig.rust_analyzer.setup {}
-        lspconfig.ts_ls.setup {}
+        lspconfig.bashls.setup { on_attach = on_attach }                                   -- Bash
+        lspconfig.fish_lsp.setup { on_attach = on_attach }                                 -- Fish
+        lspconfig.pylsp.setup { on_attach = on_attach }                                    -- Python
+        lspconfig.asm_lsp.setup { on_attach = on_attach }                                  -- Assembly
+        lspconfig.clangd.setup { on_attach = on_attach }                                   -- C/C++
+        lspconfig.rust_analyzer.setup { on_attach = on_attach }                            -- Rust
+        lspconfig.gopls.setup { on_attach = on_attach }                                    -- Go
+        lspconfig.ts_ls.setup { on_attach = on_attach }                                    -- Typescript/Javascript
+        -- lspconfig.nil_ls.setup { on_attach = on_attach }                                -- Nix
+        lspconfig.hyprls.setup { on_attach = on_attach }                                   -- Hyprlang
+        lspconfig.markdown_oxide.setup { on_attach = on_attach }                           -- Markdown
 
+        -- Lua
         lspconfig.lua_ls.setup {
+          on_attach = on_attach,
           on_init = function(client)
             if client.workspace_folders then
               local path = client.workspace_folders[1].name
@@ -71,9 +80,6 @@ return {
             }
           },
         }
-
-        lspconfig.zls.setup {}
-        vim.cmd.let "g:zig_fmt_autosave = 0"
 
         vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
             pattern = "*.wgsl",
