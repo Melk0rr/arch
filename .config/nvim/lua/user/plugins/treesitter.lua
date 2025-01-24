f21c35bf4a094558fad0060e978663b8f4adfa52
Me1k0r <runner-me1k0r@proton.me>
2025-01-24 15:44:49 +0100

    edit: nvim - make sure highlight is enabled in treesitter

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -1,24 +1,27 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "fish",
-          "go",
-          "json",
-          "lua",
-          "make",
-          "markdown",
-          "python",
-          "rust",
-          "toml",
-          "vim",
-          "yaml"
-        }
-      })
-    end
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      highlight = {
+        enable = true
+      },
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "fish",
+        "go",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "python",
+        "rust",
+        "toml",
+        "vim",
+        "yaml"
+      }
+    })
+  end
 }
