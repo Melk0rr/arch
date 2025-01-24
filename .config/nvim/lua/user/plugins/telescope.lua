f0c886fa1f2e6efdf741f0ae69d0b2c935d26096
Me1k0r <runner-me1k0r@proton.me>
2025-01-24 15:41:41 +0100

    new: nvim - telescope project plugin

--- .config/nvim/lua/user/plugins/telescope.lua
+++ .config/nvim/lua/user/plugins/telescope.lua
@@ -1,24 +1,27 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = '0.1.8',
-    config = function()
-        require("telescope").setup {
-            pickers = {
-                colorscheme = {
-                    enable_preview = true,
-                },
-            },
-            defaults = {
-                file_ignore_patterns = {
-                    'Cargo.lock',
-                    'build/',
-                    'node_modules',
-                    'target',
-                    '%.svg',
-                    'package%-lock%.json',
-                },
-            },
-        }
-    end,
-    dependencies = { "nvim-lua/plenary.nvim" },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    -- Telescope
+    require("telescope").setup {
+      pickers = {
+        colorscheme = {
+          enable_preview = true,
+        },
+      },
+      defaults = {
+        file_ignore_patterns = {
+          "Cargo.lock",
+          "build/",
+          "node_modules",
+          "target",
+          "%.svg",
+          "package%-lock%.json",
+        },
+      },
+    }
+  end,
 }
