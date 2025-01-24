9f01674e5020479fba96222cff6fc7e8ee6c8d1b
Me1k0r <runner-me1k0r@proton.me>
2025-01-24 15:47:22 +0100

    edit: nvim - base16 colorscheme customization

--- .config/nvim/lua/user/plugins/base16.lua
+++ .config/nvim/lua/user/plugins/base16.lua
@@ -2,10 +2,25 @@ return {
   "RRethy/base16-nvim",
   config = function()
     require("base16-colorscheme").setup({
-      base00 = '#2E2B2C', base01 = '#7D4B59', base02 = '#6C4B7D', base03 = '#4B627D',
-      base04 = '#7D4B5C', base05 = '#CC9AE6', base06 = '#9ABDE6', base07 = '#E69AAF',
-      base08 = '#A3657A', base09 = '#F0AABE', base0A = '#D9AAF0', base0B = '#AACAF0',
-      base0C = '#F0AAC1', base0D = '#D9AAF0', base0E = '#AACAF0', base0F = '#F0AABE',
+      base00 = '#0C0D0E', base01 = '#7D4B5A', base02 = '#4B557D', base03 = '#4B607D',
+      base04 = '#4B647D', base05 = '#9AA9E6', base06 = '#9AB9E6', base07 = '#E69AB1',
+      base08 = '#6584A3', base09 = '#F0AABF', base0A = '#AAB8F0', base0B = '#AAC7F0',
+      base0C = '#AACDF0', base0D = '#AAB8F0', base0E = '#AAC7F0', base0F = '#F0AABF',
+    })
+
+    local border = require("base16-colorscheme").colors.base01
+    local title = require("base16-colorscheme").colors.base0A
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        -- Telescope customization
+        vim.api.nvim_set_hl(0, "TelescopeBorder", { bg="NONE", fg=border })
+        vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg="NONE", fg=border })
+        vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg="NONE" })
+        vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { bg="NONE" })
+        vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg="NONE", fg=title })
+        vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg="NONE", fg=title })
+        vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { bg="NONE", fg=title })
+      end
     })
   end
 }
