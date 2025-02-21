43967a8cb8fb4f4a760fb5b84b9fcd4d7806f2b9
Me1k0r <runner-me1k0r@proton.me>
2025-02-21 15:49:32 +0100

    edit: fish - config
    cursor settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,18 @@ set -gx TERMINAL 'kitty'
 # Commands to run in interactive sessions can go here
 if status is-interactive
 	# Vim keybindings
-	fish_vi_key_bindings
+	# fish_vi_key_bindings
+
+  # Set cursor style
+  set fish_cursor_default block blink
+  set fish_cursor_insert  line  blink
+  set fish_cursor_visual  block
+  set fish_vi_force_cursor 1
+
+  function fish_user_key_bindings
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+  end
 	
 	oh-my-posh init fish --config ~/.config/oh-my-posh/omp.json | source
 
