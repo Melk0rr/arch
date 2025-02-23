9f68d7d87e8c5b682298766b06e515a4f2c66a10
Me1k0r <runner-me1k0r@proton.me>
2025-02-23 19:00:36 +0100

    edit: nvim/plugins - whichkey
    Whichkey groups and icons

--- .config/nvim/lua/user/plugins/whichkey.lua
+++ .config/nvim/lua/user/plugins/whichkey.lua
@@ -1,28 +1,43 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  opts = {
-    preset = "modern"
-  },
-  keys = {
-    { "<leader>" , group = "Leader" },
-    { "<leader>?" , function () require("which-key").show({ global = false }) end, desc = "Show local keymaps" },
+  config = function()
+    local wk = require("which-key")
+    wk.setup({ preset = "modern" })
+    wk.add({
+      -- { "<leader>" , group = "Leader" },
+      { "<leader>?" , function () require("which-key").show({ global = false }) end, desc = "Show local keymaps", icon = { icon = "󰋖", color = "yellow" } },
 
-    -- Fuzzy finder group
-    { "<leader>f", group = "Fuzzy Finder" },
+      -- NOTE: Buffer group
+      { "<leader>b", group = "Buffer" },
+      { "<leader>bb", "<cmd>bnext<cr>", desc = "Next Buffer" },
+      { "<leader>bB", "<cmd>bnext<cr>", desc = "Previous Buffer" },
 
-    -- Buffer group
-    { "<leader>b", group = "Buffer" },
-    { "gb", "<cmd>bnext<cr>", desc = "Next Buffer" },
-    { "gB", "<cmd>bnext<cr>", desc = "Previous Buffer" },
+      -- NOTE: Debug group
+      { "<leader>d", group = "Debug", icon = { icon = "󰃤", color = "red" } },
 
-    -- Git group
-    { "<leader>g", group = "Git" },
+      -- NOTE: Debug group
+      { "<leader>e", group = "Explorer", icon = { icon = "", color = "yellow" } },
 
-    -- LSP group
-    { "<leader>l", group = "LSP" },
+      -- NOTE: Fuzzy finder group
+      { "<leader>f", group = "Fuzzy Finder", icon = { icon = "󰭎", color = "blue" } },
 
-    -- Debug group
-    { "<leader>d", group = "Debug" },
-  }
+      -- NOTE: Git group
+      { "<leader>g", group = "Git", icon = { icon = "", color = "orange" } },
+
+      -- NOTE: LSP group
+      { "<leader>l", group = "LSP", icon = { icon = "", color = "green" } },
+
+      -- NOTE: Quit group
+      { "<leader>q", group = "Quit", icon = { icon = "󰈆", color = "red" } },
+
+      -- NOTE: Search group
+      { "<leader>s", group = "Search", icon = { icon = "", color = "blue" } },
+
+      -- NOTE: Terminal group
+      { "<leader>t", group = "Terminal", icon = { icon = "", color = "green" } },
+    }
+  )
+  end
 }
+
