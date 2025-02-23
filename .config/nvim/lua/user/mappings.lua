7e6351b2e32266d22c3955c2482e24fd50e680ed
Me1k0r <runner-me1k0r@proton.me>
2025-02-23 19:01:06 +0100

    edit: nvim - mappings
    Moved most of plugin mapping into snacks

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -3,18 +3,18 @@ local opts = function(desc)
   return { noremap = true, silent = true, desc = desc }
 end
 
--- Common
+-- NOTE: Common
 map('', 'Y', 'y$', opts("Yank whole line"))
 map('n', '<leader;', 'mqA;<esc>`q', opts("Semicolon at end of line"))
 map('n', '<leader>h', '<cmd>noh<cr>', opts("No search highlight"))
 map('n', '<leader>qq', '<cmd>wqa!<cr>', opts("Save and quit"))
 map('n', '<C-a>', 'goVG', opts("Select all"))
 
--- Press jk fast to exit insert mode
+-- NOTE: Press jk fast to exit insert mode
 map('i', 'jk', '<ESC>', opts("jk to exit insert mode"))
 map('i', 'kj', '<ESC>', opts("kj to exit insert mode"))
 
--- Window navigation
+-- NOTE: Window navigation
 map('n', '<C-h>', '<C-w>h', opts("Go to left window"))
 map('n', '<C-j>', '<C-w>j', opts("Go to lower window"))
 map('n', '<C-k>', '<C-w>k', opts("Go to upper window"))
@@ -24,41 +24,22 @@ map('t', '<C-j>', [[<C-\><C-n><C-w>j]], opts("Go to lower terminal"))
 map('t', '<C-k>', [[<C-\><C-n><C-w>k]], opts("Go to upper terminal"))
 map('t', '<C-l>', [[<C-\><C-n><C-w>l]], opts("Go to right terminal"))
 
--- Buffer navigation
+-- NOTE: Buffer navigation
 map('n', '<Tab>', '<cmd>bnext<cr>', opts("Next buffer"))
 map('n', '<S-Tab>', '<cmd>bprevious<cr>', opts("Previous buffer"))
 map('n', '<leader>bde', '<cmd>%bd|e#<cr>', opts("Close all buffers except the current one"))
 
--- Stay in indent mode
+-- NOTE: Stay in indent mode
 map('v', '<', '<gv^', opts("Unindent"))
 map('v', '>', '>gv^', opts("Indent"))
 
--- Move lines
+-- NOTE: Move lines
 map('n', '<A-j>', ':m .+1<cr>==', opts("Move line down"))
 map('n', '<A-k>', ':m .-2<cr>==', opts("Move line up"))
 map('v', '<A-j>', ":m '>+1<cr>gv=gv", opts("Move selection down"))
 map('v', '<A-k>', ":m '<-2<cr>gv=gv", opts("Move selection up"))
 
--- Plugin-specific
--- search
-local telescope = require("telescope.builtin")
-map('n', '<leader>ff', telescope.find_files, opts("Telescope find files"))
-map('n', '<leader>fr', telescope.oldfiles, opts("Telescope find recent files"))
-map('n', '<leader>fk', telescope.keymaps, opts("Telescope find keymaps"))
-map('n', '<leader>fC', telescope.commands, opts("Telescope find commands"))
-map('n', '<leader>fg', telescope.grep_string, opts("Telescope grep string"))
-map('n', '<leader>fG', telescope.live_grep, opts("Telescope live grep"))
-map('n', '<leader>ft', '<cmd>TodoTelescope<cr>', opts("Telescope todo comments"))
-
--- git
-map('n', '<leader>gB', telescope.git_branches, opts("Telescope git branches"))
-map('n', '<leader>gc', telescope.git_commits, opts("Telescope git commits"))
-map('n', '<leader>gs', telescope.git_status, opts("Telescope git status"))
-
--- neo-tree
-map('n', '<leader>e', '<cmd>Neotree toggle<cr>', opts("Toggle neo tree"))
-
--- Debugging
+-- NOTE: Debugging
 map('n', '<leader>dc', '<cmd>lua require("dap").continue()<cr>', opts("Debugger continue"))
 map('n', '<leader>dj', '<cmd>lua require("dap").step_over()<cr>', opts("Debugger step over"))
 map('n', '<leader>dk', '<cmd>lua require("dap").step_out()<cr>', opts("Debugger step out"))
