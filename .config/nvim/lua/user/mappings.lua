d1b3367c6dad7c99ef5239343c13a10c44ed8d25
Me1k0r <runner-me1k0r@proton.me>
2025-02-24 12:08:41 +0100

    edit: nvim - mappings
    All mappings into dedicated file

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -40,10 +40,60 @@ map('v', '<A-j>', ":m '>+1<cr>gv=gv", opts("Move selection down"))
 map('v', '<A-k>', ":m '<-2<cr>gv=gv", opts("Move selection up"))
 
 -- NOTE: Debugging
-map('n', '<leader>dc', '<cmd>lua require("dap").continue()<cr>', opts("Debugger continue"))
-map('n', '<leader>dj', '<cmd>lua require("dap").step_over()<cr>', opts("Debugger step over"))
-map('n', '<leader>dk', '<cmd>lua require("dap").step_out()<cr>', opts("Debugger step out"))
-map('n', '<leader>dl', '<cmd>lua require("dap").step_into()<cr>', opts("Debugger step into"))
-map('n', '<leader>db', '<cmd>lua require("dap").toggle_breakpoint()<cr>', opts("Debugger toggle breakpoint"))
-map('n', '<leader>dt', '<cmd>lua vim.cmd("RustLsp testables")<cr>', opts("Debugger run rust tests"))
-map('n', '<leader>dx', '<cmd>lua require("dap").terminate()<cr><cmd>lua require("dapui").close()<cr>', opts("Debugger terminate"))
+map('n', "<leader>dc", '<cmd>lua require("dap").continue()<cr>', opts("Debugger continue"))
+map('n', "<leader>dj", '<cmd>lua require("dap").step_over()<cr>', opts("Debugger step over"))
+map('n', "<leader>dk", '<cmd>lua require("dap").step_out()<cr>', opts("Debugger step out"))
+map('n', "<leader>dl", '<cmd>lua require("dap").step_into()<cr>', opts("Debugger step into"))
+map('n', "<leader>db", '<cmd>lua require("dap").toggle_breakpoint()<cr>', opts("Debugger toggle breakpoint"))
+map('n', "<leader>dt", '<cmd>lua vim.cmd("RustLsp testables")<cr>', opts("Debugger run rust tests"))
+map('n', "<leader>dx", '<cmd>lua require("dap").terminate()<cr><cmd>lua require("dapui").close()<cr>', opts("Debugger terminate"))
+
+-- NOTE: Git
+map('n', "<leader>gb", "<cmd>lua require('snacks').git.blame_line()<cr>", opts("Git blame Line"))
+map('n', "<leader>gB", "<cmd>lua require('snacks').picker.git_branches()<cr>", opts("Git branches"))
+map('n', "<leader>gd", "<cmd>lua require('snacks').picker.git_diff()<cr>", opts("Git differences"))
+map('n', "<leader>gg", "<cmd>lua require('snacks').lazygit()<cr>", opts("Lazygit"))
+map('n', "<leader>gl", "<cmd>lua require('snacks').lazygit.log()<cr>", opts("Lazygit logs"))
+map('n', "<leader>gs", "<cmd>lua require('snacks').picker.git_status()<cr>", opts("Git status"))
+
+-- NOTE: Dashboard
+map('n', "<leader>a", "<cmd>lua require('snacks').dashboard()<cr>", opts("Snacks dashboard"))
+
+-- NOTE: Explorer
+map('n', "<leader>e", "<cmd>lua require('snacks').picker.explorer()<cr>", opts("Snacks file explorer"))
+
+-- NOTE: Picker find
+map('n', "<leader>fa", "<cmd>lua require('snacks').picker.autocmds()<cr>", opts("Find autocmds"))
+map('n', "<leader>fb", "<cmd>lua require('snacks').picker.buffers()<cr>", opts("Find buffers"))
+map('n', "<leader>fB", "<cmd>lua require('snacks').picker.grep_buffers()<cr>", opts("Grep open buffers"))
+map('n', "<leader>fc", "<cmd>lua require('snacks').picker.files()<cr>", opts("Find config files"))
+map('n', "<leader>fC", "<cmd>lua require('snacks').picker.commands()<cr>", opts("Find commands"))
+map('n', "<leader>ff", "<cmd>lua require('snacks').picker.files()<cr>", opts("Find files"))
+map('n', "<leader>fg", "<cmd>lua require('snacks').picker.grep_word()<cr>", opts("Grep selection"))
+map('n', "<leader>fG", "<cmd>lua require('snacks').picker.grep()<cr>", opts("Live grep"))
+map('n', "<leader>fh", "<cmd>lua require('snacks').picker.highlights()<cr>", opts("Find highlights"))
+map('n', "<leader>fH", "<cmd>lua require('snacks').picker.help()<cr>", opts("Find help pages"))
+map('n', "<leader>fi", "<cmd>lua require('snacks').picker.icons()<cr>", opts("Find icons"))
+map('n', "<leader>fk", "<cmd>lua require('snacks').picker.keymaps({ layout = 'vertical' })<cr>", opts("Find keymaps"))
+map('n', "<leader>fM", "<cmd>lua require('snacks').picker.man()<cr>", opts("Find man pages"))
+map('n', "<leader>fp", "<cmd>lua require('snacks').picker.projects()<cr>", opts("Find projects"))
+map('n', "<leader>fr", "<cmd>lua require('snacks').picker.recent()<cr>", opts("Find recent files"))
+map('n', "<leader>fr", "<cmd>lua require('snacks').picker.todo_comments()<cr>", opts("Find Todo comments"))
+map('n', "<leader>fr", "<cmd>lua require('snacks').picker.todo_comments({ 'TODO', 'FIX', 'FIXME' })<cr>", opts("Find Todo/Fix/Fixme"))
+
+-- NOTE: Picker search
+map('n', '<leader>s"', "<cmd>lua require('snacks').picker.registers()<cr>", opts("Search registers"))
+map('n', "<leader>s:", "<cmd>lua require('snacks').picker.command_history()<cr>", opts("Search command history"))
+map('n', "<leader>s/", "<cmd>lua require('snacks').picker.search_history()<cr>", opts("Search history"))
+map('n', "<leader>su", "<cmd>lua require('snacks').picker.undo({ layout = 'vertical' })<cr>", opts("Search undo history"))
+
+-- NOTE: Terminal
+map({ 'n', 't' }, "<leader>tt", "<cmd>lua require('snacks').terminal()<cr>", opts("Toggle terminal"))
+
+-- NOTE: Words & LSP
+map('n', "<leader>ld", "<cmd>lua require('snacks').picker.diagnostics()<cr>", opts("Find LSP diagnostics"))
+map('n', "<leader>ll", "<cmd>lua require('snacks').words.jump(vim.v.count1)<cr>", opts("Next LSP reference"))
+map('n', "<leader>lL", "<cmd>lua require('snacks').words.jump(-vim.v.count1)<cr>", opts("Previous LSP reference"))
+map('n', "<leader>lR", "<cmd>lua require('snacks').picker.references()<cr>", opts("Find LSP references"))
+map('n', "<leader>ls", "<cmd>lua require('snacks').picker.lsp_symbols({ layout = 'vertical' })<cr>", opts("Find LSP symbols"))
+map('n', "<leader>lS", "<cmd>lua require('snacks').picker.lsp_workspace_symbols({ layout = 'vertical' })<cr>", opts("Find LSP workspace symbols"))
