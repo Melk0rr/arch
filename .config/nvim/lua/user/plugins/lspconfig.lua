7d6f4a46f6d9a5182bf3d842ec4ac76c560aa0b3
Me1k0r <runner-me1k0r@proton.me>
2025-02-28 17:13:11 +0100

    edit: nvim/plugins - lspconfig
    Diagnostic signs

--- .config/nvim/lua/user/plugins/lspconfig.lua
+++ .config/nvim/lua/user/plugins/lspconfig.lua
@@ -127,9 +127,15 @@ return {
               [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
             },
           },
-        }
       },
     })
+
+    -- HACK: Change diagnostics symbols
+    local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
 
     vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
       pattern = "*.wgsl",
