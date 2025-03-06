8b605f3911bd93bd82d582d5a3e952d389263319
Me1k0r <runner-me1k0r@proton.me>
2025-03-06 12:06:15 +0100

    edit: .config/nvim - proper submodule
    Properly added nvim as a submodule

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-local home = os.getenv 'HOME'
-package.path = package.path .. ';' .. home .. '/.config/nvim/lua?.lua'
-vim.opt.runtimepath:append(home .. '/.config/nvim/')
-
-require("user.options")
-require("user.lazy")
-require("user.autocmd")
-require("user.mappings")
