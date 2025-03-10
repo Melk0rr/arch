9615cef05194e05013108173166339493a4e9a5e
me1k0r <runner-me1k0r@proton.me>
2025-03-10 10:04:45 +0100

    edit: wsl - wsl branch
    Deleted useless dotfiles for WSL

--- .local/share/bin/songdetail.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-song_info=$(playerctl metadata --format '{{artist}}      {{title}}')
-
-echo "$song_info" 
--- .local/share/bin/wallbashkdeglobals.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-sed -i "/^BackgroundNormal=/c\BackgroundNormal=#132025" "$HOME/.config/kdeglobals"
