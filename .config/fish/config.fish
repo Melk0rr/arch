9338cd1522af9d325393bddd9a393f8eda2f4f63
Me1k0r <runner-me1k0r@proton.me>
2025-04-10 11:29:25 +0200

    edit: .config/fish - organization + yay rebuild abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -121,6 +121,7 @@ if status is-interactive
   # alias vim='nvim'
 	alias grep='grep --color'
 
+  # NOTE: General system monitoring / maintenance
 	# Install date
 	abbr install-date 'stat -c %w / | cut -b 1-16'
 
@@ -132,10 +133,18 @@ if status is-interactive
 	abbr psmem 'ps auxf | sort -nr -k 4'
 	abbr pscpu 'ps auxf | sort -nr -k 3'
 	abbr jctl 'journalctl -p 3 -xb'
+	abbr lsblk 'lsblk -o +uuid,name'
+
+	# PLocate
+	alias locate='plocate'
 
 	# Patching
 	abbr patch-file 'diff -Naru'
 	abbr patch-dir 'diff -crB'
+
+	# DNS
+	abbr dnstls-opt 'sudo sed -i "/^DNSOverTLS=/c\DNSOverTLS=opportunistic" /etc/systemd/resolved.conf; sudo systemctl restart systemd-resolved'
+	abbr dnstls-yes 'sudo sed -i "/^DNSOverTLS=/c\DNSOverTLS=yes" /etc/systemd/resolved.conf; sudo systemctl restart systemd-resolved'
 
 	# LS
 	alias ls='eza -la --color=always --group-directories-first --icons'
@@ -143,25 +152,16 @@ if status is-interactive
 	alias ll='eza -l --color=always --group-directories-first --icons'
 	alias lt='eza -aT --color=always --group-directories-first --icons'
 
-	# PLocate
-	alias locate='plocate'
-
-	# Pacman
-	# abbr list-updates 'checkupdates; yay -Qua'
+	# NOTE: Arch / Pacman specific
 	abbr clean-arch 'yay -Sc && yay -Yc'
 	abbr clean-orphans 'pacman -Qtdq | sudo pacman -Rns -'
 	abbr uninstall 'sudo pacman -Rns'
+  abbr rebuild 'yay -S --answerclean All --rebuild'
 	abbr update-mirrors 'sudo reflector --verbose --score 100 --latest 20 --fastest 5 --sort rate --save /etc/pacman.d/mirrorlist'
 	abbr fix-key 'sudo rm /var/lib/pacman/sync/* && sudo rm -rf /etc/pacman.d/gnupg/* && sudo pacman-key --init && sudo pacman-key --populate && sudo pacman -Sy --noconfirm archlinux-keyring && sudo pacman --noconfirm -Su'
 	abbr chroot-build 'mkdir -p ~/Documents/chroot/; set CHROOT $HOME/Documents/chroot; mkarchroot $CHROOT/root base-devel; makechrootpkg -c -r $CHROOT'
-	abbr lsblk 'lsblk -o +uuid,name'
-	
-	#DNS
-	abbr dnstls-opt 'sudo sed -i "/^DNSOverTLS=/c\DNSOverTLS=opportunistic" /etc/systemd/resolved.conf; sudo systemctl restart systemd-resolved'
-	abbr dnstls-yes 'sudo sed -i "/^DNSOverTLS=/c\DNSOverTLS=yes" /etc/systemd/resolved.conf; sudo systemctl restart systemd-resolved'
 
-	# Rclone
-	abbr rcc 'rclone copy'
+  # NOTE: Dev
 
 	# Git & dev
 	abbr gin 'git init'
@@ -180,16 +180,19 @@ if status is-interactive
 	abbr gb 'git branch'
 	abbr gre 'git rebase'
 
+
+	alias py='python3'
+
+  # NOTE: QoL
+	# Zoxide
+	zoxide init --cmd cd fish | source
+
 	# yt-dlp
 	abbr ytdl 'yt-dlp --output "%(title)s.%(ext)s"'
 	abbr ytdlp 'yt-dlp --audio-format mp3 -i --output "%(playlist_index)s-%(title)s.%(ext)s"'
 	abbr ytdla 'yt-dlp --audio-format mp3 -i -x -f bestaudio/best --output "%(playlist_index)s-%(title)s.%(ext)s"'
 
-	alias py='python3'
-
-
-	# Zoxide
-	zoxide init --cmd cd fish | source
-
+	# Rclone
+	abbr rcc 'rclone copy'
 end
 # End of interactive mode
