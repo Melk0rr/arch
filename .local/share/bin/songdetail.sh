f15ad3b8e1bcb30d84ceba3272714c65095173cf
Me1k0r <runner-me1k0r@proton.me>
2025-04-12 13:54:13 +0200

    edit: scripts/songdetail - icon based on playerctl status

--- .local/share/bin/songdetail.sh
+++ .local/share/bin/songdetail.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
-song_info=$(playerctl metadata --format '{{artist}}      {{title}}')
+player_status=$(playerctl status)
+
+icon=$(case $player_status in
+    "Playing") echo "" ;;
+    "Paused")  echo "" ;;
+    *)         echo "" ;;
+esac)  
+
+song_info=$(playerctl metadata --format "$icon   {{artist}}   -   {{title}}")
 
 echo "$song_info" 
