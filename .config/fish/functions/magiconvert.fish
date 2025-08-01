8675a00e2f60798b2383c4ca4b9df866e383a9da
Me1k0r <runner-me1k0r@proton.me>
2025-08-01 11:31:39 +0200

    fix: .config/fish/f/magiconvert - missing --argument

--- .config/fish/functions/magiconvert.fish
+++ .config/fish/functions/magiconvert.fish
@@ -1,4 +1,4 @@
 # Convert the input image into desired format and delete the old one
-function magiconvert file file_cvt
+function magiconvert --argument file file_cvt
   magick $file $file_cvt && rm $file
 end
